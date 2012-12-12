fc66f66bf761e6ea5cfb544ec9216983c113c473
XROW System Account <xrow@test01.hande.c.de.siteprotect.eu>
2012-12-12 11:33:14 +0100

    translations added

---
+++ translations/pol-PL/translation.ts
@@ -0,0 +1,34 @@
+<!DOCTYPE TS>
+<TS><context>
+    <name>design/xrowcaptcha</name>
+    <message>
+      <source>Security challenge</source>
+      <translation>Mechanizm zabezpieczeń</translation>
+      
+    </message>
+    <message>
+      <source>required</source>
+      <translation>wymagane</translation>
+      
+    </message>
+    <message>
+      <source>Please solve the following challenge.</source>
+      <translation>Przejdź weryfikację.</translation>
+      
+    </message>
+    <message>
+      <source>New challenge</source>
+      <translation>Nowy obrazek</translation>
+      
+    </message>
+    <message>
+      <source>Challenge successfully solved.</source>
+      <translation>Weryfikacja przebiegła pomyślnie.</translation>
+      
+    </message>
+    <message>
+      <source>Challenge not solved.</source>
+      <translation>Nie udało się zweryfikować.</translation>
+      
+    </message>
+  </context></TS>
