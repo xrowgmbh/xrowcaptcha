d047425a6b03f41ca813e809f60c14a3b3578f35
xrowrobert <robert@xrow.de>
2013-10-08 13:04:48 +0200

    - advancedsearch added to exclude list

--- settings/xrowcaptcha.ini.append.php
+++ settings/xrowcaptcha.ini.append.php
@@ -5,7 +5,9 @@ ExcludeURLs[]=/user/login
 ExcludeURLs[]=/user/register
 ExcludeURLs[]=/ezjscore/call/
 ExcludeURLs[]=/content/search
+ExcludeURLs[]=/content/advancedsearch
 ExcludeURLs[]=/content/edit
 ExcludeURLs[]=/setup/cachetoolbar
 ExcludeURLs[]=/setup/settingstoolbar
+
 */ ?>
