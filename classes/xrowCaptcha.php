d0a70d48ef7e4e1ee63f29546f15ad08ff4af95d
xrow <bjoern@xrow.de>
2014-01-24 11:07:34 +0100

    formated

--- classes/xrowCaptcha.php
+++ classes/xrowCaptcha.php
@@ -4,14 +4,14 @@ class xrowCaptcha
     public static function isTrusted()
     {
         $http = eZHTTPTool::instance();
-        if ( $http->sessionVariable('xrowCaptchaSolved', '0' ) === '1' )
+        if ( $http->sessionVariable( 'xrowCaptchaSolved', '0' ) === '1' )
         {
             return true;
         }
         elseif ( eZUser::instance()->isLoggedIn() )
         {
-            $threedays= time() - 3600 * 72;
-            if ( eZUser::instance()->lastVisit() > $threedays)
+            $threedays = time() - 3600 * 72;
+            if ( eZUser::instance()->lastVisit() > $threedays )
             {
                 return true;
             }
@@ -33,11 +33,11 @@ class xrowCaptcha
         $second_var = rand( 0, 9 );
         $operator = ( rand( 0, 1 ) == 0 ) ? ' + ' : ' - ';
         $exercise = $first_var . $operator . $second_var;
-        eval("\$result = " . $exercise . ';');
-        $vars['exercise'] = array('var1' => $first_var,
-                                  'var2' => $second_var,
-                                  'operator' => $operator,
-                                  'result' => $result
+        eval( "\$result = " . $exercise . ';' );
+        $vars['exercise'] = array( 'var1' => $first_var,
+                                   'var2' => $second_var,
+                                   'operator' => $operator,
+                                   'result' => $result
                                   );
         return $vars;
     }
